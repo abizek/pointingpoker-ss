62f393ad1fd7f9765748ad6bf24fbf76a2924c55
abizek <abizekgh@protonmail.com>
2022-05-01 20:26:21 +0530

    feat: handle disconnect, reconnect and add option to kick disconnected players

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,21 @@
 // TODO: hide votes until shown with show votes button
 import { Fragment, useRef, useState, useEffect } from 'react'
 import { useSyncedStore } from '@syncedstore/react'
-import { store, wsProvider } from './store'
+import { store, wsProvider, awareness, clientID } from './store'
 
 const App = () => {
+  const sharedState = useSyncedStore(store)
+
   const [name, setName] = useState(window.localStorage.getItem('name') ?? '')
-  const setPersistedName = (nameValue) => {
+  const setPersistedName = nameValue => {
+    if (nameValue === '') return
     window.localStorage.setItem('name', nameValue)
     setName(nameValue)
+    sharedState.players[nameValue] = {
+      vote: null,
+      active: true,
+      clientID
+    }
   }
 
   const inputRef = useRef()
@@ -21,23 +29,51 @@ const App = () => {
   const [connected, setConnected] = useState(false)
 
   useEffect(() => {
-    wsProvider.on('status', event => {
-      console.log(event.status) // logs "connected" or "disconnected"
-      if (event.status === 'connected') {
-        setConnected(true)
-      } else {
-        setConnected(false)
+    const handleWSConnectionStatusChange = ({ status }) => {
+      console.log(status) // logs "connected" or "disconnected"
+      const isConnected = status === 'connected'
+      setConnected(isConnected)
+    }
+    wsProvider.on('status', handleWSConnectionStatusChange)
+
+    const handleWSSync = isSynced => {
+      if (isSynced && name) {
+        // initialise player object on reentry
+        sharedState.players[name] = {
+          vote: null,
+          active: true,
+          clientID // to associate clientID with player object
+        }
       }
-      // TODO: handle disconnected event
-      // TODO: add player active flag, modify it on page-lifecycle and add option to kick user
-    })
-  }, [])
+    }
+    wsProvider.on('sync', handleWSSync)
 
-  const sharedState = useSyncedStore(store)
+    const handleAwarenessUpdate = ({ removed }) => {
+      removed.forEach(removedClientId => {
+        const removedPlayer = Object.keys(sharedState.players).find(
+          player => sharedState.players[player].clientID === removedClientId
+        )
+        if (removedPlayer) {
+          sharedState.players[removedPlayer].active = false
+        }
+      })
+    }
+    awareness.on('update', handleAwarenessUpdate)
 
-  const setVote = (voteValue) => {
+    return () => {
+      wsProvider.off('status', handleWSConnectionStatusChange)
+      wsProvider.off('sync', handleWSSync)
+      awareness.off('update', handleAwarenessUpdate)
+    }
+  }, [sharedState.players])
+
+  const setVote = voteValue => {
     // TODO: persist vote
-    sharedState.players[name] = voteValue
+    sharedState.players[name].vote = voteValue
+  }
+
+  const kickPlayer = playerName => {
+    delete sharedState.players[playerName]
   }
 
   return (
@@ -66,16 +102,23 @@ const App = () => {
             </form>
           </>
           )}
-      {connected && Object.keys(sharedState.players).map(playerName => {
-        const vote = sharedState.players[playerName]
-        return (
-          <Fragment key={playerName}>
-            <br />
-            {playerName + ': ' + vote}
-          </Fragment>
-        )
-      }
-      )}
+      {connected &&
+        Object.keys(sharedState.players).map(playerName => {
+          const vote = sharedState.players[playerName].vote
+          const active = sharedState.players[playerName].active
+          return (
+            playerName && (
+              <Fragment key={playerName}>
+                <br />
+                {`${playerName}${active ? '' : '(left)'}: ${vote}`}
+                &nbsp;
+                {!active && (
+                  <button onClick={() => kickPlayer(playerName)}>Kick</button>
+                )}
+              </Fragment>
+            )
+          )
+        })}
     </>
   )
 }
