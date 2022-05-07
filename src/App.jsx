68dc1f41c2182e0978a98ac3eaa816b345ff3488
abizek <91976421+abizek@users.noreply.github.com>
2022-05-07 23:01:03 +0530

    feat: custom room support
    refactor: rooms logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,18 @@
 // TODO: add styles
 // TODO: dark mode
-import { Fragment, useState, useEffect } from 'react'
+import { Fragment, useState } from 'react'
 import { Form, PointingPokerSection } from './components'
-import { joinRoom } from './room'
 
 const App = () => {
   // TODO: maybe add random session generator?
-  // TODO: revisit name state logic
-  const [name, setName] = useState('')
-
-  useEffect(() => {
-    const room = window.localStorage.getItem('room')
-    if (room) {
-      ;(async () => {
-        await joinRoom(room)
-        setName(window.localStorage.getItem('name') ?? '')
-      })()
-    }
-  }, [])
+  const [name, setName] = useState(window.localStorage.getItem('name') ?? '')
+  const [roomJoined, setRoomJoined] = useState(false)
 
   return (
     <>
-      {name ? <PointingPokerSection name={name} /> : <Form setName={setName} />}
+      {name && roomJoined
+        ? <PointingPokerSection name={name} />
+        : <Form name={name} setName={setName} setRoomJoined={setRoomJoined} />}
     </>
   )
 }
--- src/room.js
+++ src/room.js
@@ -3,7 +3,9 @@ import { yDoc } from './store'
 
 export const room = { wsProvider: null, awareness: null, clientID: null }
 
-export const joinRoom = async roomName => {
+export const joinRoom = roomName => {
+  if (roomName === '') return
+
   // TODO: use webrtc provider and use WS/Turn as fallback
   room.wsProvider = new WebsocketProvider(
     'wss://floating-reef-83846.herokuapp.com', // TODO: use env var
